b1194a015982b2e02ee6fd2166b9234e4a90b2ac
Christian Urich <christian.urich@gmail.com>
2022-02-24 19:04:48 +1100

    fix(d2j): fix dynaind export

--- src/scenario_tool_interface/dynamind2json.py
+++ src/scenario_tool_interface/dynamind2json.py
@@ -37,15 +37,24 @@ class DynaMindXML2Json:
 
             filter = n.find('Filter')
             if filter:
-                filter_view = [e.text.strip() for e in filter.findall("view_name")]
-                attribtue_filter = [e.text.strip() if e.text else "" for e in filter.findall("attribtue_filter")]
-                spatial_filter = [e.text.strip() if e.text else "" for e in filter.findall("spatial_filter")]
+                try:
+                    filter_view = [e.text.strip() for e in filter.findall("view_name")]
+                except AttributeError:
+                    filter_view = []
+                try:
+                    attribtue_filter = [e.text.strip() if e.text else "" for e in filter.findall("attribtue_filter")]
+                except AttributeError:
+                    attribtue_filter = []
+                try:
+                    spatial_filter = [e.text.strip() if e.text else "" for e in filter.findall("spatial_filter")]
+                except AttributeError:
+                    spatial_filter = []
                 node_filter = {}
                 node_filter["view_name"] = filter_view
                 node_filter["attribute_filter"] = attribtue_filter
                 node_filter["spatial_filter"] = spatial_filter
                 node["filter"] = node_filter
-                # print(node["name"], node_filter)
+
             group_uuids.add(node["group_uuid"])
             node["parameters"] = self._generate_parameters(n)
             self._nodes[n.find('UUID').get("value")] = node
